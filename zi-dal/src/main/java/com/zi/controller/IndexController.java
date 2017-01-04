b31619e227d71bfa4b6098e5df22d1640dfae1b7
WangziwenUnique <1994.7.28Bb>
2017-01-04 17:18:58 +0800

    登录页面

--- zi-dal/src/main/java/com/zi/controller/IndexController.java
+++ zi-dal/src/main/java/com/zi/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.zi.controller;
 import com.zi.sys.factory.ServiceImplFactory;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController extends ServiceImplFactory {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    /**
+     * 动态页面跳转接口
+     *
+     * @param url
+     * @return
+     */
     @RequestMapping("{url}")
-    public String dynamicUrl(@PathVariable String url){
-        System.out.print("进入动态URL方法");
+    public String dynamicUrl(@PathVariable String url) {
+        logger.info("页面跳转：{}", url);
         return url;
     }
 }
