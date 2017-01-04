571d552b957d18fa15bdac971994eec168edbffc
WangziwenUnique <1050598666@qq.com>
2017-01-04 17:12:37 +0800

    登录页面

--- zi-dal/src/main/java/com/zi/controller/IndexController.java
+++ zi-dal/src/main/java/com/zi/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.zi.sys.factory.ServiceImplFactory;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,34 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController extends ServiceImplFactory {
-
-    /**
-     * 登录跳转
-     *
-     * @return
-     */
-    @RequestMapping("login")
-    public String login() {
-        return "login";
-    }
-
-    /**
-     * 未授权页面跳转
-     *
-     * @return
-     */
-    @RequestMapping("unauthor")
-    public String unauthor() {
-        return "unauthor";
-    }
-
-    /**
-     * 主页面跳转
-     *
-     * @return
-     */
-    @RequestMapping("index")
-    public String index() {
-        return "index";
+    @RequestMapping("{url}")
+    public String dynamicUrl(@PathVariable String url){
+        System.out.print("进入动态URL方法");
+        return url;
     }
 }
